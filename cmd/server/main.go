9409f80ec3fcf79548615fb1896d0b6fac6a31a8
Eric Masi <emasi@vitusvet.com>
2023-09-25 21:15:53 -0400

    Add basic server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	log.Println("Let's go!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
+	server := http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	log.Printf("Listening on %v", server.Addr)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
