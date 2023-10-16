2f9d4b7220dd53e324a57812f2fb2f693902371d
Eric Masi <emasi@vitusvet.com>
2023-10-16 11:55:48 -0400

    Fix bug, handle scientific notation

--- cmd/dataviz/main.go
+++ cmd/dataviz/main.go
@@ -91,26 +91,26 @@ func readMetrics(scanner *bufio.Scanner) (map[string]int, map[string]int, error)
 			before, after, _ := strings.Cut(line, " ")
 
 			time := latencyRegex.FindString(before)
-			numRequests, err := strconv.Atoi(after)
+			numRequests, err := strconv.ParseFloat(after, 32)
 
 			if err != nil {
 				return nil, nil, err
 			}
 
-			bm[time] = numRequests
+			bm[time] = int(numRequests)
 		}
 
 		if strings.Contains(line, "json_message_latency_milliseconds_bucket") {
 			before, after, _ := strings.Cut(line, " ")
 
 			time := latencyRegex.FindString(before)
-			numRequests, err := strconv.Atoi(after)
+			numRequests, err := strconv.ParseFloat(after, 32)
 
 			if err != nil {
 				return nil, nil, err
 			}
 
-			jm[time] = numRequests
+			jm[time] = int(numRequests)
 		}
 	}
 
